10b051a6be1619d8d44ac661c9a34a5d0fc38372
unknown <wlo@wlo_win8.wsidomain.wasoftware.com>
2015-06-19 11:54:08 -0400

    Initial commit

---
+++ src/main/java/com/wasoftware/bpa/BpaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.wasoftware.bpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpaSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BpaSpringBootApplication.class, args);
+    }
+}
+d
---
+++ src/main/java/example/HelloWorldClient.java
@@ -0,0 +1,8 @@
+package example;/**
+ * Created by wlo on 6/18/2015.
+ */
+public class HelloWorldClient {
+  public static void main(String[] argv) {
+
+  }
+}
